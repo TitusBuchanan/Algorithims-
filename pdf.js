42f4099b33b9f5d9d7a2731359d74d9767cb446f
titus buchanan <titusbuchananjr@gmail.com>
2020-08-20 18:40:08 -0400

    completed a problem solving challenge

---
+++ pdf.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the designerPdfViewer function below.
+function designerPdfViewer(h, word) {
+    
+   
+    var alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    var word = 'zaba';
+    
+
+    let letterHeight = []
+    
+
+    for ( let i = 0;  i < word.length; i++) {
+        letterHeight.push(h[alphabet.indexOf(word[i])])
+    }
+
+    let tallest = letterHeight.sort()
+    [letterHeight.length - 1]
+
+
+    return tallest * word.length
+
+    
+}
+
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const h = readLine().split(' ').map(hTemp => parseInt(hTemp, 10));
+
+    const word = readLine();
+
+    let result = designerPdfViewer(h, word);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
