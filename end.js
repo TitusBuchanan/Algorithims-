b2874fa13cf3baeae628b881665dea9529ee9bbc
titus buchanan <titusbuchananjr@gmail.com>
2020-08-27 18:40:50 -0400

    refactored code

--- end.js
+++ end.js
@@ -1,10 +1,10 @@
 function confirmEnding(str, target) {
-    // "Never give up and good luck will find you."
-    // -- Falcor
-  
-    let re = new RegExp(target + "$", "i");
-  
-    return re.test(str);
-  }
-  
-  console.log(confirmEnding("Bastian", "n"));
+  // "Never give up and good luck will find you."
+  // -- Falcor
+
+  let reg = new RegExp(target + "$", "i");
+
+  return reg.test(str);
+}
+
+console.log(confirmEnding("Bastian", "n"));
